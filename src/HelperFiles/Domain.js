8435b120b42199349bc91016a680a8eea12e13be
Shivam <sharmashivam1918@gmail.com>
2025-04-04 06:47:30 +0530

    images path issues

--- src/HelperFiles/Domain.js
+++ src/HelperFiles/Domain.js
@@ -1,3 +1,3 @@
-const Domain = "http://localhost:5000";
-// const Domain = "https://krishibackend.up.railway.app";
+// const Domain = "http://localhost:5000";
+const Domain = "https://krishibackend.up.railway.app";
 export default Domain;
